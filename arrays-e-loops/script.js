d4096a86d8bb634fbf716701f169a657207c0851
Raissa Campos <raissa.campos26@gmail.com>
2021-10-07 07:52:07 -0300

    feat: initial arrays and loops

---
+++ arrays-e-loops/script.js
@@ -0,0 +1,17 @@
+const videoGames = ['Switch', "Ps4", "Xbox"];
+
+const ultimoItem = videoGames.pop() //Remove o último item e retorna ele
+console.log("🚀 ~ file: script.js ~ line 4 ~ ultimoItem", ultimoItem)
+
+const addItem = videoGames.push('3DS'); // Adiciona ao final da array
+console.log("🚀 ~ file: script.js ~ line 7 ~ addItem", addItem)
+
+for (let numero = 0; numero < 10; numero++) {
+console.log("🚀 ~ file: script.js ~ line 10 ~ numero", numero)
+}
+
+var i = 0;
+while (i < 10) {
+ console.log("🚀 ~ file: script.js ~ line 15 ~ i", i++);
+}
+
